3e48bd8a01aa70f2cacb9c6eb73ed4e6803a3164
Kyrylo Onyshchenko <cyril.onyshchenko@gmail.com>
2026-06-05 02:13:07 +0300

    Lecture 6

--- src/cloud-functions/check-token/main.py
+++ src/cloud-functions/check-token/main.py
@@ -8,18 +8,24 @@ cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
 
 @functions_framework.http
-def protected():
+def protected(request):
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+
     auth_header = request.headers.get('Authorization', '')
     if not auth_header.startswith('Bearer '):
-        return jsonify({'error': 'No token provided'}), 401
+        return (jsonify({'error': 'No token provided'}), 401, headers)
 
     id_token = auth_header.split('Bearer ')[1]
     try:
         decoded_token = auth.verify_id_token(id_token)
-        return jsonify({
+        return (jsonify({
             'message': 'Access granted',
             'user': decoded_token.get('email'),
             'uid': decoded_token.get('uid')
-        }), 200
+        }), 200, headers)
     except Exception as e:
-        return jsonify({'error': 'Unauthorized', 'details': str(e)}), 401
+        return (jsonify({'error': 'Unauthorized', 'details': str(e)}), 401, headers)
