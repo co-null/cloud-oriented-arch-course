c6594e53c4915303737701f5c1bdd27197ee88ba
Kyrylo Onyshchenko <cyril.onyshchenko@gmail.com>
2026-06-05 04:45:07 +0300

    Lecture 6

--- src/cloud-functions/check-token/main.py
+++ src/cloud-functions/check-token/main.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, auth
 import functions_framework
 from flask import jsonify, request
+import logging
 
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
@@ -19,16 +20,20 @@ def protected(request):
         return ('', 204, headers)
 
     auth_header = request.headers.get('Authorization', '')
+    logging.info(f"Authorization header: {auth_header}")
     if not auth_header.startswith('Bearer '):
+        logging.error("No Bearer token")
         return (jsonify({'error': 'No token provided'}), 401, headers)
 
     id_token = auth_header.split('Bearer ')[1]
     try:
         decoded_token = auth.verify_id_token(id_token)
+        logging.info(f"Decoded token: {decoded_token}")
         return (jsonify({
             'message': 'Access granted',
             'user': decoded_token.get('email'),
             'uid': decoded_token.get('uid')
         }), 200, headers)
     except Exception as e:
+        logging.error(f"Token verification error: {str(e)}")
         return (jsonify({'error': 'Unauthorized', 'details': str(e)}), 401, headers)
