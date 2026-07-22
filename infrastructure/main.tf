a74f40b75cca25b116a9bab4bcd1861db188d736
Kyrylo Onyshchenko <cyril.onyshchenko@gmail.com>
2026-07-23 02:14:41 +0300

    Lecture 10

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -68,27 +68,6 @@ resource "google_project_iam_member" "function_sa_firestore_user" {
   member  = "serviceAccount:${google_service_account.pubsub_function_sa.email}"
 }
 
-# Додаткові права для Firestore (якщо потрібно створювати колекції)
-resource "google_project_iam_member" "function_sa_firestore_owner" {
-  project = var.project_id
-  role    = "roles/datastore.owner"
-  member  = "serviceAccount:${google_service_account.pubsub_function_sa.email}"
-}
-
-# Права для Firebase Admin SDK
-resource "google_project_iam_member" "function_sa_firebase_admin" {
-  project = var.project_id
-  role    = "roles/firebase.admin"
-  member  = "serviceAccount:${google_service_account.pubsub_function_sa.email}"
-}
-
-# Project-level IAM для Service Accounts
-resource "google_project_iam_member" "function_sa_pubsub_admin" {
-  project = var.project_id
-  role    = "roles/pubsub.admin"
-  member  = "serviceAccount:${google_service_account.pubsub_function_sa.email}"
-}
-
 resource "google_project_iam_member" "function_sa_logging" {
   project = var.project_id
   role    = "roles/logging.logWriter"
@@ -117,11 +96,6 @@ resource "google_service_account_iam_member" "functions_sa_user" {
 
 # Додаткові права для функцій
 # Права на створення підписок
-resource "google_project_iam_member" "function_sa_subscription_admin" {
-  project = var.project_id
-  role    = "roles/pubsub.admin"
-  member  = "serviceAccount:${google_service_account.pubsub_function_sa.email}"
-}
 
 # Створення токену для авторизації для функції диспетчера
 resource "google_project_iam_member" "function_sa_token_creator" {
