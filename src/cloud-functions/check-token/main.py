1265abab242060696c015aa0f00d3774000c17f4
Kyrylo Onyshchenko <cyril.onyshchenko@gmail.com>
2026-07-31 12:26:47 +0300

    Lecture 6

--- src/cloud-functions/check-token/main.py
+++ src/cloud-functions/check-token/main.py
@@ -1,13 +1,24 @@
-import firebase_admin, os
+import firebase_admin
 from firebase_admin import credentials, auth
 import functions_framework
-from flask import jsonify, request
-import logging
+import logging, json
 
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
-    'projectId': 'cloud-oriented-arch-course'
+    'projectId': 'cloud-oriented-arch-course'  # Replace with your actual project ID
 })
+
+def make_json_response(payload, status_code, cors_headers):
+    headers = {
+        **cors_headers,
+        'Content-Type': 'application/json',
+    }
+
+    return (
+        json.dumps(payload),
+        status_code,
+        headers,
+    )
 
 @functions_framework.http
 def protected(request):
@@ -25,17 +36,17 @@ def protected(request):
     logging.info(f"Authorization header: {auth_header}")
     if not auth_header.startswith('Bearer '):
         logging.error("No Bearer token")
-        return (jsonify({'error': 'No token provided'}), 401, headers)
+        return make_json_response({'error': 'No token provided'}, 401, headers)
 
     id_token = auth_header.split('Bearer ')[1]
     try:
         decoded_token = auth.verify_id_token(id_token)
         logging.info(f"Decoded token: {decoded_token}")
-        return (jsonify({
+        return (make_json_response({
             'message': 'Access granted',
             'user': decoded_token.get('email'),
             'uid': decoded_token.get('uid')
         }), 200, headers)
     except Exception as e:
         logging.error(f"Token verification error: {str(e)}")
-        return (jsonify({'error': 'Unauthorized', 'details': str(e)}), 401, headers)
+        return (make_json_response({'error': 'Unauthorized', 'details': str(e)}), 401, headers)
